afaee336a9fdc26fa942294572b42aab53f30857
Chris Hundhausen <hundhaus@wsu.edu>
2021-09-28 16:11:56 -0700

    Renamed js folder to scripts

---
+++ scripts/script1.js
@@ -0,0 +1,8 @@
+const Hello = () => {
+    return React.createElement('h1', null, 'Hello World');
+  }
+    
+  ReactDOM.render(
+    React.createElement(Hello),
+    document.getElementById('root')
+  );  
