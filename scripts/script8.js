4b6e99e59fef2222add55331d99beb8e52b1d638
Chris Hundhausen <hundhaus@wsu.edu>
2021-09-28 17:19:49 -0700

    Add script8.js

---
+++ scripts/script8.js
@@ -0,0 +1,17 @@
+function Clock(props) {
+    return (
+      <div>
+        <h1>Hello React Students!</h1>
+        <h2>It is now {props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+}
+
+function updateTime() {
+    ReactDOM.render(
+      <Clock date={new Date()} />,
+      document.getElementById('root')
+    );
+  }
+
+  setInterval(updateTime, 1000);
